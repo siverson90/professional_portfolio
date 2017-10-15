eed7efaa4540171b9ef98cf367a2a1da2ecada3d
Joshua Siverson <joshuasiverson@Joshuas-MacBook-Air.local>
2017-10-15 15:36:16 -0700

    adding logos for languges section and animation

--- javascript/logic.js
+++ javascript/logic.js
@@ -1,20 +1,67 @@
 $( document ).ready(function() {
   
-  $('.carousel').carousel({
-  // interval: 4000
-  })
-
+  // Arrays and Arrays of Objects
   var hobbbiesArray = [ 
-  "Crossfit", 
-  "travel internationally", 
-  "eat Japanese Ramen", 
-  "attend tech meetups", 
-  "listen to podcasts",
-  "attend coffee tastings",
+    "Crossfit", 
+    "travel internationally", 
+    "eat Japanese Ramen", 
+    "attend tech meetups", 
+    "listen to podcasts",
+    "attend coffee tastings",
   ];
-  
+
+  var projects = [
+    {
+      Name : "Liri Bot",
+      Descritption : "This project was great",
+      Image : "images/robot.png",
+      imageLink: "https://github.com/siverson90/LIRI-Bot",
+    },
+    {
+      Name : "Train Schedule",
+      Descritption : "This project was great",
+      Image : "images/train.jpg",
+      imageLink: "https://github.com/siverson90/LIRI-Bot",
+    },
+    {
+      Name: "Bored on a Friday",
+      Descritption: "This project was great",
+      Image: "images/fun.jpg",
+      imageLink: "https://github.com/siverson90/LIRI-Bot",
+    }
+  ];
+
+  var languages = [
+    {
+      name : "HTML",
+      image : "images/html5.png",
+    },
+    {
+      name : "CSS",
+      image : "images/css3.png",
+    },
+    {
+      name : "Javascript",
+      image : "images/javascript.png",
+    },
+    {
+      name : "Jquery",
+      image : "images/jquery.png",
+    },
+    {
+      name : "Node.JS",
+      image : "images/nodejs.png",
+    },
+    {
+      name : "Bootstrap",
+      image : "images/bootstrap.png",
+    }
+  ]
+
+  // Profile picture, bio and interests
+ 
   var hobbyCounter=0;
-  // var myVar = setInterval(renderHobby, 3000);
+  var myVar = setInterval(renderHobby, 3000);
 
   function renderHobby() {
       
@@ -29,41 +76,17 @@ $( document ).ready(function() {
 
   }
 
-  // add onclick event listener and allow user to download resume
-  $('#resume').click(function(event) {
-    event.preventDefault();  //stop the browser from following
-    window.location.href = '../Resume_Josh_Siverson.pdf';
-  });
+  $('.carousel').carousel({
+    interval: 4000
+  })
 
-
-  // Create object of project to iterate through and render to screen
-
-  var projects = [
-  {
-    Name : "Liri Bot",
-    Descritption : "This project was great",
-    Image : "images/fun.jpg",
-    imageLink: "https://github.com/siverson90/LIRI-Bot",
-  },
-  {
-    Name : "Train Schedule",
-    Descritption : "This project was great",
-    Image : "images/fun.jpg",
-    imageLink: "https://github.com/siverson90/LIRI-Bot",
-  },
-  {
-    Name: "Bored on a Friday",
-    Descritption: "This project was great",
-    Image: "images/fun.jpg",
-    imageLink: "https://github.com/siverson90/LIRI-Bot",
-  }
-  ];
+  // Projects
 
   projects.forEach(function(element) {
     var carouselDiv = $("<div>");
-    var rowDiv = $("<div>");
-    var pDiv = $("<div>").addClass("col-md-4");
-    var imageDiv = $("<div>").addClass("col-md-8");
+    var rowDiv = $("<div>").addClass("row");
+    var pDiv = $("<div>").addClass("col-md-6");
+    var imageDiv = $("<div>").addClass("col-md-4");
     var projectName = $("<p>");
     var projectDes = $("<p>");
     var projectImage = $("<img>");
@@ -73,7 +96,7 @@ $( document ).ready(function() {
     projectDes.text(element.Descritption);
     projectImage.attr("src", element.Image);
     projectImage.attr("alt", "="+element.Name);
-    projectImage.addClass("d-block img-fluid");
+    projectImage.addClass("d-block img-fluid carousel-image");
     
     imageAHref.append(projectImage);
     imageDiv.append(imageAHref);
@@ -95,17 +118,33 @@ $( document ).ready(function() {
     $(".carousel-item:eq(1)").addClass("active");
   }
 
-// <div class="carousel-item active">
-//       <div class="row">
-//          <!-- wraps write up about project and image -->
-//         <div class="col-md-4">
-//           <p>This project was great!</p>
-//         </div>
-//         <div class="col-md-8">
-//           <a href="https://github.com/siverson90/LIRI-Bot">
-//            <img class="d-block img-fluid" src="images/fun.jpg" alt="First slide">
-//           </a>
-//         </div>
-//       </div>
-//     </div>
+  // Resume, langaues and Social
+
+  // add onclick event listener and allow user to download resume
+  $('#resume').click(function(event) {
+    event.preventDefault();  //stop the browser from following
+    window.location.href = '../Resume_Josh_Siverson.pdf';
+  });
+
+  // Iterate through array of objects to render languages learned
+  languages.forEach(function(element){
+    var bootstrapDiv = $("<div>").addClass("col-md-4");
+    var img = $("<img>").addClass("img-fluid language-logo rounded");
+    img.attr("src", element.image);
+    img.attr("alt", element.name);
+
+    bootstrapDiv.append(img);
+    $("#logos-row").append(bootstrapDiv);
+
+  })
+
+  $(".language-logo").mouseover(function(){
+    $(this).addClass("animated shake");
+  })
+
+  $(".language-logo").mouseleave(function(){
+    $(this).removeClass("animated shake");
+  })
+
 });
+
